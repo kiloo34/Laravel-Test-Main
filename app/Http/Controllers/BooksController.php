1455a90393085452a75168a6dc6cb1bc5ea0c5b1
revamp sobatbangun <revamp.sobatbangun@gmail.com>
2022-09-12 13:26:14 +0700

    answer question github version

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Author;
 use App\Book;
 use App\Http\Requests\PostBookRequest;
 use App\Http\Resources\BookResource;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
 
 class BooksController extends Controller
 {
@@ -16,16 +19,69 @@ class BooksController extends Controller
 
     public function index(Request $request)
     {
-        // @TODO implement
+        $book_query = Book::with('reviews');
+        if ($request->authors) {
+            $author = explode(',', $request->authors);
+            $book_query->whereHas('authors', function ($q) use ($author) {
+                $q->whereIn('id', $author);
+            });
+        }
+
+        // done list
+        if ($request->sortColumn) {
+            if ($request->sortColumn == 'avg_review') {
+                $book_query->withCount(['reviews as avg_review' => function ($q) {
+                    $q->select(\DB::raw('avg(review)'));
+                }]);
+                if ($request->sortDirection) {
+                    $book_query->orderBy($request->sortColumn, $request->sortDirection);
+                } else {
+                    $book_query->orderBy($request->sortColumn);
+                }
+            } elseif ($request->sortDirection) {
+                $book_query->orderBy($request->sortColumn, $request->sortDirection);
+            } else {
+                $book_query->orderBy($request->sortColumn);
+            }
+        }
+
+        $books = $book_query->paginate(15);
         
-        return BookResource::collection();
+        return BookResource::collection($books);
     }
 
     public function store(PostBookRequest $request)
     {
-        // @TODO implement
+        $validator = Validator::make($request->all(), 
+        [
+            'isbn'              => ['required', 'unique:books', 'size:13'],
+            'title'             => ['required'],
+            'description'       => ['required'],
+            'authors'           => ['required', 'array', 'exists:authors,id'],
+            'authors.*'         => ['required', 'exists:authors,id'],
+            'published_year'    => ['required', 'digits:4', 'integer', 'between:1900,'.(date('Y')-1)],
+        ]);
+
+        if($validator->fails()) {
+            dd($validator->validate());
+            return response()->json($validator->validate(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        
+        $author = Author::find(1);
         $book = new Book();
+
+        // book section
+        $book->isbn = $request->input('isbn');
+        $book->title = $request->input('title');
+        $book->description = $request->input('description');
+        $book->published_year = 2000;
+        $book->description = $request->input('description');
+        $book->save();
+        // authors section
+        $book->authors()->attach([1]);
 
         return new BookResource($book);
     }
+
+
 }
--- app/Http/Controllers/BooksReviewController.php
+++ app/Http/Controllers/BooksReviewController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Book;
 use App\BookReview;
 use App\Http\Requests\PostBookReviewRequest;
 use App\Http\Resources\BookReviewResource;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
 
 class BooksReviewController extends Controller
 {
@@ -14,10 +18,36 @@ class BooksReviewController extends Controller
 
     }
 
+    public function show($id)
+    {
+        # code...
+    }
+
     public function store(int $bookId, PostBookReviewRequest $request)
     {
         // @TODO implement
+        $book = new Book(); // important
+        $book->findOrFail($bookId); // important
+
+        $validator = Validator::make($request->all(), 
+        [
+            'review'    => ['required', 'numeric', 'integer', 'between:1,10'],
+            'comment'   => ['required'],
+        ]);
+
+        if($validator->fails()) {
+            dd($validator->validate());
+            return response()->json($validator->validate(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
         $bookReview = new BookReview();
+
+        // bookreview section
+        $bookReview->book_id = 1;
+        $bookReview->user_id = 1;
+        $bookReview->review = $request->input('review');
+        $bookReview->comment = $request->input('comment');
+        $bookReview->save();
 
         return new BookReviewResource($bookReview);
     }
@@ -25,5 +55,16 @@ class BooksReviewController extends Controller
     public function destroy(int $bookId, int $reviewId, Request $request)
     {
         // @TODO implement
+        $data = BookReview::where([
+            'id' => $reviewId,
+            'book_id' => $bookId
+        ])->first();
+        // dd($data);
+        if ($data) {
+            $data->delete();
+            return response()->json([], Response::HTTP_NO_CONTENT);
+        } else {
+            return response()->json([], Response::HTTP_NOT_FOUND);
+        }
     }
 }
